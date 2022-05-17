d3c70e32591ed030436bfa279fd6a67f18db0188
Brandon Steven Pérez Caamaño <brandons.perez.caamano@gmail.com>
2022-05-17 12:14:30 -0400

    tarea Brandon

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 // 1. Crea una function que tome como parametro un array y devuelva la suma de sus
 // elementos.
+
+function sumatoria(numeropositivo = [1, 2, 3, 4, 5, 6, 7, 8, 9], numeronegativo = [-1,-3, -2, -6, -7, -9, -8, -4, -5]){  
+    return numeropositivo.length + numeronegativo.length;
+}
+ let resultado = sumatoria()
+ console.log(resultado)
+
